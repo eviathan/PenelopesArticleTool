205f944e368447a22c478c3f42184da2d9eb16b0
Brian Williams <emial.brianwilliams@gmail.com>
2019-02-20 23:07:38 +0000

    INIT

---
+++ webPages/www.mushroom-magazine.com/site/wp-content/plugins/imager/assets/js/frontend.js
@@ -0,0 +1,24 @@
+/* 
+File: assets/js/frontend.js
+Description: JS Script for Frontend
+Plugin: Imager
+Author: Alessio Marzo
+*/
+
+jQuery(function($){
+	$(document).ready(function() { 
+
+
+	/*
+	var config = {
+	"opacity": 60,
+	"className": "watermark",
+	"position": "bottomright",
+	"path": "http://dev.ad-theme.com/alessio/wp-content/uploads/2013/12/ws_cc_profile.jpg" };
+	
+	$(document).watermark(config);
+	 */
+
+
+});
+});
